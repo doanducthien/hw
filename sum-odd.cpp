cabf1b05ef8948f2a3875115713dd6d90081e916
doanducthien <35688231+doanducthien@users.noreply.github.com>
2018-02-01 13:40:00 +0700

    Update sum-odd.cpp

--- sum-odd.cpp
+++ sum-odd.cpp
@@ -2,16 +2,16 @@
 using namespace std;
 int main()
 {
-  int a,b;
+  int a,b,i;
   int sum = 0;
   cin>>a>>b;
   for (i = a;i<=b;i++)
-  (
+  {
     if (i % 2 != 0)
-    (
+    {
     sum = sum + i;
-    )
-  )
+    }
+  }
   cout<<sum<<endl;
   return 0;
 }
